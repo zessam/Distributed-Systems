6ab3803a0510870af48f18ef777cbb285e7d8909
Zeyad Essam <72177003+zezo-rgb@users.noreply.github.com>
2023-03-15 16:42:52 +0200

    adding constants class

--- 0x00-TCP/src/constants.java
+++ 0x00-TCP/src/constants.java
@@ -1,6 +1,5 @@
-
-
-
 public class constants {
-    public static 
+    public static final String IP = "127.0.0.1";
+    public static final int PORT = 5554;
+    public static final String STOP = "exit";
 }
--- 0x00-TCP/src/server.java
+++ 0x00-TCP/src/server.java
@@ -1,3 +1,24 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class server {
-    
-}
+    private ServerSocket serversocket = null ; // socket for the server
+    private Socket socket = null ; // socket for the client
+    private DataInputStream input = null;  // I stream
+    private DataOutputStream output = null; // O stream
+    Scanner scan;
+
+    public server()
+    {
+    scan = new Scanner(System.in);
+    }
+
+    private void openInputStream()  // open Input stream 
+    {
+        
+    }
+
+
